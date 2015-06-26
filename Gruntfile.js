d46e80fc997916766975637d30eda2466024ce94
FrendEr <frend.wong@gmail.com>
2015-06-26 20:28:04 +0800

    add scroll direction tips

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,28 @@
 module.exports = function(grunt) {
+    grunt.initConfig({
+    	jshint: {
+    		src: 'src/fScrollspy.js'
+    	},
 
+    	copy: {
+    		main: {
+    			src: 'src/fScrollspy.js',
+    			dest: 'dist/fScrollspy.js'
+    		}
+    	},
+
+    	uglify: {
+    		file: {
+    			src: 'src/fScrollspy.js',
+    			dest: 'dist/fScrollspy.min.js'
+    		}
+    	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('js', ['copy', 'uglify']);
+    grunt.registerTask('default', ['js']);
 }
